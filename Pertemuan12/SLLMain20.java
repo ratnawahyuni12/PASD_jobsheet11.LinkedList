30386eb9b78d218e1acf8633caec6437c3b3267e
ratnawahyuni12 <ratna120307@gmail.com>
2026-05-06 09:52:17 +0700

    2.1 — Modifikasi : pertanyaan 2.1 nomor 3

--- Pertemuan12/SLLMain20.java
+++ Pertemuan12/SLLMain20.java
@@ -1,21 +1,27 @@
 package Pertemuan12;
 
+import java.util.Scanner;
+
 public class SLLMain20 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList20 sll = new SingleLinkedList20();
 
-        Mahasiswa20 mhs1 = new Mahasiswa20("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa20 mhs2 = new Mahasiswa20("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa20 mhs3 = new Mahasiswa20("23212201", "Bimon",  "2B", 3.8);
-        Mahasiswa20 mhs4 = new Mahasiswa20("21212203", "Dirga",  "4D", 3.6);
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt(); sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("=== Data Mahasiswa ke-" + (i+1) + " ===");
+            System.out.print("NIM   : "); String nim = sc.nextLine();
+            System.out.print("Nama  : "); String nama = sc.nextLine();
+            System.out.print("Kelas : "); String kelas = sc.nextLine();
+            System.out.print("IPK   : "); double ipk = sc.nextDouble(); sc.nextLine();
+
+            Mahasiswa20 mhs = new Mahasiswa20(nim, nama, kelas, ipk);
+            sll.addLast(mhs);
+        }
 
         sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(1, mhs2);
-        sll.print();
+        sc.close();
     }
 }
