c4d4bf2261e01de6ee4b9490f43b164cda04116d
ratnawahyuni12 <ratna120307@gmail.com>
2026-05-06 10:41:31 +0700

    2.2 — Percobaan 1 : Modifikasi Elemen pada Single Linked List

--- Pertemuan12/SLLMain20.java
+++ Pertemuan12/SLLMain20.java
@@ -1,27 +1,32 @@
 package Pertemuan12;
 
-import java.util.Scanner;
-
 public class SLLMain20 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         SingleLinkedList20 sll = new SingleLinkedList20();
-
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int n = sc.nextInt(); sc.nextLine();
-
-        for (int i = 0; i < n; i++) {
-            System.out.println("=== Data Mahasiswa ke-" + (i+1) + " ===");
-            System.out.print("NIM   : "); String nim = sc.nextLine();
-            System.out.print("Nama  : "); String nama = sc.nextLine();
-            System.out.print("Kelas : "); String kelas = sc.nextLine();
-            System.out.print("IPK   : "); double ipk = sc.nextDouble(); sc.nextLine();
-
-            Mahasiswa20 mhs = new Mahasiswa20(nim, nama, kelas, ipk);
-            sll.addLast(mhs);
-        }
-
+ 
+        Mahasiswa20 mhs1 = new Mahasiswa20("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa20 mhs2 = new Mahasiswa20("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa20 mhs3 = new Mahasiswa20("23212201", "Bimon",  "2B", 3.8);
+        Mahasiswa20 mhs4 = new Mahasiswa20("21212203", "Dirga",  "4D", 3.6);
+ 
+        sll.addFirst(mhs4);
+        sll.addLast(mhs1);
+        sll.insertAfter("Dirga", mhs3);
+        sll.insertAt(1, mhs2);
+        
+        System.out.println("Kondisi awal: ");
         sll.print();
-        sc.close();
+        
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+ 
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+ 
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
